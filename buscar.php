4283a198fa00590dc994895971d7cf221ae8f605
cireexe <celina.cc1501@gmail.com>
2023-12-14 13:47:03 -0300

    bd actualizacion del hosting

--- buscar.php
+++ buscar.php
@@ -1,9 +1,9 @@
 <?php
 // Configuración de la conexión a la base de datos
 $host = "localhost";
-$usuario = "root";
-$contrasena = "";
-$base_de_datos = "blog";
+$usuario = "id21671733_celina";
+$contrasena = "Cbc.45422691";
+$base_de_datos = "id21671733_blog";
 
 $conexion = new mysqli($host, $usuario, $contrasena, $base_de_datos);
 
