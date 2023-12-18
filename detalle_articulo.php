ec0fd4ab97f30d81d5b61a1a758bb530242b7b56
cireexe <celina.cc1501@gmail.com>
2023-12-18 12:06:15 -0300

    actualizacion titulos

--- detalle_articulo.php
+++ detalle_articulo.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Detalle del Artículo - Tu Blog de Estilo de Vida</title>
+    <title>Tu Blog de Estilo de Vida</title>
 
     <style>
         body {
